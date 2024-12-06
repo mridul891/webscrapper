4279b928e88b6c36f039e898fe076a8f6930bdab
Mridul Pandey <pandeym891@gmail.com>
2024-12-07 05:20:38 +0530

    scrapper chamges

--- sih.js
+++ sih.js
@@ -13,6 +13,7 @@ async function scrapeNews(req, res) {
     const url = "https://mridul891.github.io/newssite/";
 
     // Fetch and scrape the website
+    const cities = ["Kalpa", "Mumbai ", "Kerala", "Bhopal", "Guwahati"];
     axios
       .get(url)
       .then((response) => {
@@ -25,7 +26,12 @@ async function scrapeNews(req, res) {
         // Assuming headlines are in <h2> tags
         $("h2").each((index, element) => {
           const headline = $(element).text().replace(/\s+/g, " ").trim(); // Trim removes extra spaces and newlines
-          headlines.push({ title: headline });
+          const city = Math.floor(Math.random()*10)%cities.length
+          console.log(city)
+          headlines.push({
+            title: headline,
+            city: cities[city]
+          });
         });
 
         // Log the extracted data
@@ -39,6 +45,43 @@ async function scrapeNews(req, res) {
   }
 }
 
+
+async function scrapeTwitter(req, res) {
+  try {
+    const url = "https://mridul891.github.io/newssite/";
+
+    // Fetch and scrape the website
+    const cities = ["Kalpa", "Mumbai ", "Kerala", "Bhopal", "Guwahati"];
+    axios
+      .get(url)
+      .then((response) => {
+        // Load the HTML into cheerio
+        const $ = cheerio.load(response.data);
+
+        // Extract data (modify selectors based on actual HTML structure)
+        const headlines = [];
+
+        // Assuming headlines are in <h2> tags
+        $("h2").each((index, element) => {
+          const headline = $(element).text().replace(/\s+/g, " ").trim(); // Trim removes extra spaces and newlines
+          const city = Math.floor(Math.random()*10)%cities.length
+          console.log(city)
+          headlines.push({
+            title: headline,
+            city: cities[city]
+          });
+        });
+
+        // Log the extracted data
+        res.json(headlines);
+      })
+      .catch((error) => {
+        console.error("Error fetching the website:", error);
+      });
+  } catch (error) {
+    console.error("Error scraping news:", error);
+  }
+}
 // Call the function
 
 app.get("/info", scrapeNews);
